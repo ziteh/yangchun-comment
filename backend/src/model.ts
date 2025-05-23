57f97ed44c09d29ef9d0edd536b4c314a14921de
ziteh <honmonoh@gmail.com>
2025-05-23 23:14:36 +0800

    feat: update comment type

--- backend/src/model.ts
+++ backend/src/model.ts
@@ -1,16 +1,22 @@
 export type Comment = {
-  // Unique ID
+  /** Unique ID for the comment */
   id: string;
 
-  // Author name
+  /** Author name */
   name?: string;
 
-  // Author email
+  /** Author email */
   email?: string;
 
-  // Content
+  /** Content */
   msg: string;
 
-  // Publish date timestamp
+  /** Publish timestamp */
   pubDate: number;
+
+  /** ID of the comment this is a reply to */
+  replyTo?: string;
+
+  /** Is this comment by an admin? */
+  isAdmin?: boolean;
 };
