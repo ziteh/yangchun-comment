12c7e951a8bd3dba4f24108d8f1f18e648d6c0bb
ziteh <honmonoh@gmail.com>
2025-06-01 00:24:53 +0800

    feat: enhance sanitize function to handle non-string input

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -3,17 +3,26 @@ import { customAlphabet } from 'nanoid';
 import sanitizeHtml from 'sanitize-html';
 
 export default class Utils {
-  static sanitize(raw: string) {
-    const sanitized = sanitizeHtml(raw, {
+  static sanitize(raw: unknown): string {
+    if (typeof raw !== 'string') return '';
+
+    const htmlRemoved = sanitizeHtml(raw, {
       allowedTags: [], // no tags allowed
       allowedAttributes: {}, // no attributes allowed
       disallowedTagsMode: 'discard', // or 'completelyDiscard'
       parser: {
-        decodeEntities: true, // It is recommended to never disable the 'decodeEntities' option
+        // If set to true, entities within the document will be decoded. Defaults to true.
+        // It is recommended to never disable the 'decodeEntities' option
+        decodeEntities: true,
         lowerCaseTags: true,
       },
     });
-    return sanitized.replace(/\]\(\s*javascript:[^)]+\)/gi, ']('); // protection against markdown javascript links
+
+    // src: https://github.com/facebook/react/blob/v18.2.0/packages/react-dom/src/shared/sanitizeURL.js#L22
+    const isJavaScriptProtocol =
+      /^[\u0000-\u001F ]*j[\r\n\t]*a[\r\n\t]*v[\r\n\t]*a[\r\n\t]*s[\r\n\t]*c[\r\n\t]*r[\r\n\t]*i[\r\n\t]*p[\r\n\t]*t[\r\n\t]*\:/i;
+
+    return htmlRemoved.replace(/\]\(\s*javascript:[^)]+\)/gi, '](');
   }
 
   static genId() {
