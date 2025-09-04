cb4333d300375045544c1bca00fd5779ef09739a
ZiTe <honmonoh@gmail.com>
2025-09-04 21:22:51 +0800

    chore: add ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    rules: {
+      eqeqeq: ['error', 'always'],
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+  eslintConfigPrettier,
+);
