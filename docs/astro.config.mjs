5fb5f43a3eed4dda5186ac10dfdf9e702e89ebd6
ziteh <honmonoh@gmail.com>
2025-09-24 06:59:28 +0800

    chore(docs): update styles

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -23,6 +23,42 @@ export default defineConfig({
           autogenerate: { directory: 'reference' },
         },
       ],
+      head: [
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com',
+            crossorigin: '',
+          },
+        },
+        {
+          tag: 'link',
+          attrs: {
+            href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Roboto+Mono:ital,wght@0,400,600;1,400,600&display=swap',
+            rel: 'stylesheet',
+          },
+        },
+      ],
+      customCss: ['./src/styles/custom.css'],
+      expressiveCode: {
+        // https://expressive-code.com/guides/themes/
+        themes: ['one-dark-pro', 'catppuccin-latte'],
+        defaultProps: {
+          wrap: false,
+        },
+        styleOverrides: {
+          codeFontFamily:
+            "'Roboto Mono', SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
+        },
+      },
     }),
   ],
 });
