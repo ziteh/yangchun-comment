955027a9333835d220feb7119cd9644abaaf9231
ziteh <honmonoh@gmail.com>
2025-09-24 19:25:15 +0800

    ci: remove ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig(
   {
-    ignores: ['**/node_modules/**', '**/dist/**', '**/.wrangler/**'],
+    ignores: ['**/node_modules/**', '**/dist/**', '**/.wrangler/**', '**/*.astro/**'],
     extends: [eslint.configs.recommended, tseslint.configs.strict, tseslint.configs.stylistic],
     rules: {
       eqeqeq: ['error', 'always'],
