0c633826ec757cf977b45798753dba85c76acf12
ZiTe <honmonoh@gmail.com>
2026-01-04 21:27:24 +0800

    feat: add styles to comment list component

--- client/src2/components/list/comment-list.ts
+++ client/src2/components/list/comment-list.ts
@@ -1,11 +1,24 @@
-import { LitElement, html } from 'lit';
+import { LitElement, html, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import type { Comment } from '@ziteh/yangchun-comment-shared';
 import './comment-list-item';
 import { sortCommentsByDate, filterRootComments, findReplyComments } from '../../utils/comment';
+import { yangChunCommentStyles } from '../yangchun-comment.styles';
 
 @customElement('comment-list')
 export class CommentList extends LitElement {
+  static styles = [
+    yangChunCommentStyles,
+    css`
+      :host {
+        display: block;
+      }
+      .reply-comments-container {
+        margin-bottom: var(--ycc-spacing-m);
+      }
+    `,
+  ];
+
   static properties = {
     comments: { type: Array },
   };
@@ -13,7 +26,7 @@ export class CommentList extends LitElement {
 
   render() {
     return html`
-      <div>
+      <div class="reply-comments-container">
         ${sortCommentsByDate(filterRootComments(this.comments), true).map(
           (comment) => html`
             <comment-list-item
